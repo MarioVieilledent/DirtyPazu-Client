3e69aed2298ea294b2fdda86553d3a97fae4ab43
Mario <mario.vieilledent@gmail.com>
2021-11-30 00:41:33 +0100

    Stable, début du codage du line chart f(temps)

--- src/app/main/dibi-infos/dibi-infos.component.ts
+++ src/app/main/dibi-infos/dibi-infos.component.ts
@@ -14,14 +14,14 @@ export class DibiInfosComponent implements OnInit {
 
   // Stats de répartition des natures gr.
   nbPartsOfSpeech: any = [
-    { label: 'Noms', savedAs: 'Noun', nb: 0 , color: '#8AE'},
-    { label: 'Pronoms', savedAs: 'Pronoun', nb: 0 , color: '#8EE'},
-    { label: 'Verbes', savedAs: 'Verb', nb: 0 , color: '#E88'},
-    { label: 'Adjectifs', savedAs: 'Adjective', nb: 0 , color: '#8E8'},
-    { label: 'Adverbes', savedAs: 'Adverb', nb: 0 , color: '#EA8'},
-    { label: 'Conjonctions', savedAs: 'Conjonction', nb: 0 , color: '#E8E'},
+    { label: 'Noms', savedAs: 'Noun', nb: 0, color: '#8AE' },
+    { label: 'Pronoms', savedAs: 'Pronoun', nb: 0, color: '#8EE' },
+    { label: 'Verbes', savedAs: 'Verb', nb: 0, color: '#E88' },
+    { label: 'Adjectifs', savedAs: 'Adjective', nb: 0, color: '#8E8' },
+    { label: 'Adverbes', savedAs: 'Adverb', nb: 0, color: '#EA8' },
+    { label: 'Conjonctions', savedAs: 'Conjonction', nb: 0, color: '#E8E' },
     { label: 'Particules', savedAs: 'Particule', nb: 0, color: '#EE8' },
-    { label: 'Interjections', savedAs: 'Interjection', nb: 0 , color: '#A8E'}
+    { label: 'Interjections', savedAs: 'Interjection', nb: 0, color: '#A8E' }
   ]
 
   // Options Charts
@@ -35,11 +35,27 @@ export class DibiInfosComponent implements OnInit {
   }];
 
   // Pie Chart pour le nb de mots en fonction du temps
+  numbersOfWords = {
+    Noms: 0,
+    Pronoms: 0,
+    Verbes: 0,
+    Adjectifs: 0,
+    Adverbes: 0,
+    Conjonctions: 0,
+    Particules: 0,
+    Interjections: 0
+  };
   barChartData = [
-    {data: [65, 59, 80, 81, 56, 55, 40], label: 'Series A'},
-    {data: [28, 48, 40, 19, 86, 27, 90], label: 'Series B'}
+    { data: [], label: 'Noms', borderColor: '#8AE', backgroundColor: 'rgba(0, 0, 0, 0)', pointBackgroundColor: '#8AE' },
+    { data: [], label: 'Pronoms', borderColor: '#8EE', backgroundColor: 'rgba(0, 0, 0, 0)', pointBackgroundColor: '#8EE' },
+    { data: [], label: 'Verbes', borderColor: '#E88', backgroundColor: 'rgba(0, 0, 0, 0)', pointBackgroundColor: '#E88' },
+    { data: [], label: 'Adjectifs', borderColor: '#8E8', backgroundColor: 'rgba(0, 0, 0, 0)', pointBackgroundColor: '#8E8' },
+    { data: [], label: 'Adverbes', borderColor: '#EA8', backgroundColor: 'rgba(0, 0, 0, 0)', pointBackgroundColor: '#EA8' },
+    { data: [], label: 'Conjonctions', borderColor: '#E8E', backgroundColor: 'rgba(0, 0, 0, 0)', pointBackgroundColor: '#E8E' },
+    { data: [], label: 'Particules', borderColor: '#EE8', backgroundColor: 'rgba(0, 0, 0, 0)', pointBackgroundColor: '#EE8' },
+    { data: [], label: 'Interjections', borderColor: '#A8E', backgroundColor: 'rgba(0, 0, 0, 0)', pointBackgroundColor: '#A8E' }
   ];
-  barChartLabels = ['2006', '2007', '2008', '2009', '2010', '2011', '2012'];
+  barChartLabels = [];
   barChartOptions = {
     scaleShowVerticalLines: false,
     responsive: true
@@ -51,24 +67,40 @@ export class DibiInfosComponent implements OnInit {
   ngOnInit(): void {
 
     // Demande du dictionnaire
-    this.socket.emit('fetchDict', { });
+    this.socket.emit('fetchDict', {});
 
     // Récupération du dictionnaire
     this.socket.on('loadDict', (data) => {
       let list = data.dict;
       this.dibiDict = list;
 
-      // Récupérations des stats de la répartition des natures gr.
+      // Récupérations des stats de la répartition des natures gr. et nb mots langue
       this.dibiDict.forEach(word => {
         switch (word.partOfSpeech) {
-          case('Noun'): this.nbPartsOfSpeech.find(i => i.savedAs === 'Noun').nb++; break;
-          case('Pronoun'): this.nbPartsOfSpeech.find(i => i.savedAs === 'Pronoun').nb++; break;
-          case('Verb'): this.nbPartsOfSpeech.find(i => i.savedAs === 'Verb').nb++; break;
-          case('Adjective'): this.nbPartsOfSpeech.find(i => i.savedAs === 'Adjective').nb++; break;
-          case('Adverb'): this.nbPartsOfSpeech.find(i => i.savedAs === 'Adverb').nb++; break;
-          case('Conjonction'): this.nbPartsOfSpeech.find(i => i.savedAs === 'Conjonction').nb++; break;
-          case('Interjection'): this.nbPartsOfSpeech.find(i => i.savedAs === 'Interjection').nb++; break;
-          case('Particule'): this.nbPartsOfSpeech.find(i => i.savedAs === 'Particule').nb++; break;
+          case ('Noun'):
+            this.nbPartsOfSpeech.find(i => i.savedAs === 'Noun').nb++;
+            break;
+          case ('Pronoun'):
+            this.nbPartsOfSpeech.find(i => i.savedAs === 'Pronoun').nb++;
+            break;
+          case ('Verb'):
+            this.nbPartsOfSpeech.find(i => i.savedAs === 'Verb').nb++;
+            break;
+          case ('Adjective'):
+            this.nbPartsOfSpeech.find(i => i.savedAs === 'Adjective').nb++;
+            break;
+          case ('Adverb'):
+            this.nbPartsOfSpeech.find(i => i.savedAs === 'Adverb').nb++;
+            break;
+          case ('Conjonction'):
+            this.nbPartsOfSpeech.find(i => i.savedAs === 'Conjonction').nb++;
+            break;
+          case ('Particule'):
+            this.nbPartsOfSpeech.find(i => i.savedAs === 'Particule').nb++;
+            break;
+          case ('Interjection'):
+            this.nbPartsOfSpeech.find(i => i.savedAs === 'Interjection').nb++;
+            break;
         }
       });
 
@@ -90,12 +122,71 @@ export class DibiInfosComponent implements OnInit {
         this.pieChartColors[0].backgroundColor.push(element.color);
       });
 
-      // Récupérations des stats de la répartition des natures gr.
+      // Construction du line chart du nb mots chaque jour
+      let day = this.datify(new Date(this.dibiDict[0].date));
+      this.barChartLabels.push(day);
       this.dibiDict.forEach(word => {
+        if (day === this.datify(new Date(word.date))) {
+          this.numbersOfWords[this.frenchify(word.partOfSpeech)]++; // Incrément du nombre de mots selon le nature gr.
+        } else {
+          // Ajout des nouveaux mots selon la date
+          this.barChartData.forEach(elem => {
+            elem.data.push(this.numbersOfWords[elem.label]);
+          });
+          // Mise à jour de la date suivante
+          day = this.datify(new Date(word.date));
+          this.barChartLabels.push(day);
+        }
+      });
+      // Ajout des nouveaux mots selon la date
+      this.barChartData.forEach(elem => {
+        elem.data.push(this.numbersOfWords[elem.label]);
+      });
 
+      // Tri par taille (stats de répartiotion des natures gr.)
+      this.barChartData.sort((a, b) => {
+        if (a.data[a.data.length - 1] < b.data[b.data.length - 1]) {
+          return 1;
+        } else if (a.data[a.data.length - 1] > b.data[b.data.length - 1]) {
+          return -1;
+        } else {
+          return 0;
+        }
       });
 
     });
+  }
+
+  /**
+   * Adapte une Date en format dd/mm/yyy
+   */
+  datify(date: Date): string {
+    let dd: any = date.getDate();
+    let mm: any = date.getMonth() + 1;
+    let yyyy: any = date.getFullYear();
+    if (dd < 10) {
+      dd = '0' + dd;
+    }
+    if (mm < 10) {
+      mm = '0' + mm;
+    }
+    return dd + '/' + mm + '/' + yyyy;
+  }
+
+  /**
+   * Donne la nature grammaticale fr à partir de l'anglaise
+   */
+  frenchify(partOfSpeech: string): string {
+    switch (partOfSpeech) {
+      case ('Noun'): return 'Noms'; break;
+      case ('Pronoun'): return 'Pronoms'; break;
+      case ('Verb'): return 'Verbes'; break;
+      case ('Adjective'): return 'Adjectifs'; break;
+      case ('Adverb'): return 'Adverbes'; break;
+      case ('Conjonction'): return 'Conjonctions'; break;
+      case ('Particule'): return 'Particules'; break;
+      case ('Interjection'): return 'Interjections'; break;
+    }
   }
 
 }
