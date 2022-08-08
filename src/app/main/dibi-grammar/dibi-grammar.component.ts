1db7995fb7da408a3db8b1d6c7be73f4750fb1d3
Mario <mario.vieilledent@gmail.com>
2022-08-08 14:49:02 +0200

    Edition du manuel de grammaire par admins

--- src/app/main/dibi-grammar/dibi-grammar.component.ts
+++ src/app/main/dibi-grammar/dibi-grammar.component.ts
@@ -1,15 +1,27 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, Input, OnInit, ViewChild, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-dibi-grammar',
   templateUrl: './dibi-grammar.component.html',
-  styleUrls: ['./dibi-grammar.component.scss']
+  styleUrls: ['./dibi-grammar.component.scss'],
+  encapsulation: ViewEncapsulation.None
 })
 export class DibiGrammarComponent implements OnInit {
+
+  @ViewChild('body') body: ElementRef;
+  @Input() adminConnected: boolean; // Si un administrateur est connecté
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  ngAfterViewInit() {
+    fetch('assets/HTML/grammar.html')
+      .then(r => r.text())
+      .then(data => {
+        this.body.nativeElement.innerHTML = data;
+      });
   }
 
 }
